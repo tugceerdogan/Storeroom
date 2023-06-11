1d1bde0e739b4f9c444db7c226a71eb87f153a6c
Tugcenur.erdogan <tugcenur.erdogan@trendyol.com>
2023-06-12 01:43:56 +0300

    Edited home screen

---
+++ app/src/main/java/com/example/storeroom/ui/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.storeroom.ui.home
+
+import androidx.lifecycle.ViewModel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+) : ViewModel() {
+
+}
