5b2a56679f03217814a2920971787e0ef3dc06de
Tugcenur.erdogan <tugcenur.erdogan@trendyol.com>
2023-10-24 18:49:38 +0300

    Added category list screen

--- app/src/main/java/com/example/storeroom/ui/categoryList/CategoryListScreen.kt
+++ app/src/main/java/com/example/storeroom/ui/categoryList/CategoryListScreen.kt
@@ -1,6 +1,7 @@
 package com.example.storeroom.ui.categoryList
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,25 +11,74 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.example.storeroom.ui.home.HomeViewModel
 import com.example.storeroom.util.StoreroomTheme
 
 @Composable
-fun CategoryListScreen(navHostController: NavHostController) {
+fun CategoryListScreen(
+    navHostController: NavHostController,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+
+    val categories = viewModel.categories.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(300.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = "CATEGORY LIST SCREEN",
+            text = "CATEGORIES",
             style = MaterialTheme.typography.body1.copy(
                 color = Color.Black,
                 fontSize = 25.sp,
                 fontFamily = StoreroomTheme.customBoldFont
             ),
+            textAlign = TextAlign.Start,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        AllCategoriesList(categories = categories.value)
+    }
+}
+
+@Composable
+fun AllCategoriesList(categories: List<String?>) {
+    LazyColumn {
+        items(categories) {
+            CategoryItem(item = it)
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(item: String?) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(
+            text = item ?: "",
             textAlign = TextAlign.Center,
         )
     }
+}
+
+@Preview
+@Composable
+fun AllCategoriesList() {
+    AllCategoriesList(listOf("Category Name", "Category Name"))
+}
+
+@Preview
+@Composable
+fun PreviewCategoryItem() {
+    CategoryItem("Category Name")
 }
--- app/src/main/java/com/example/storeroom/ui/categoryList/CategoryListViewModel.kt
+++ app/src/main/java/com/example/storeroom/ui/categoryList/CategoryListViewModel.kt
@@ -1,9 +1,29 @@
 package com.example.storeroom.ui.categoryList
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.storeroom.domain.link.GetLinkUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CategoryListViewModel @Inject constructor() : ViewModel() {
+class CategoryListViewModel @Inject constructor(
+    private val getLinkUseCase: GetLinkUseCase
+) : ViewModel() {
+
+    val categories = MutableStateFlow<List<String?>>(emptyList())
+
+    init {
+        fetchCategoriesForUser()
+    }
+
+    fun fetchCategoriesForUser() {
+        viewModelScope.launch {
+            val user = getLinkUseCase()
+            val categoryList = user.map { it.category }
+            categories.value = categoryList
+        }
+    }
 }
