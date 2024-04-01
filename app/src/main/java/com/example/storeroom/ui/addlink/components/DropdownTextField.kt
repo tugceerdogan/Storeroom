79b8bf9e50375c62b9d908621a4cea5bbf47062d
Tugcenur.erdogan <tugcenur.erdogan@trendyol.com>
2024-04-01 11:09:35 +0300

    Add padding to DropDownMenu

--- app/src/main/java/com/example/storeroom/ui/addlink/components/DropdownTextField.kt
+++ app/src/main/java/com/example/storeroom/ui/addlink/components/DropdownTextField.kt
@@ -8,9 +8,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.DropdownMenu
@@ -28,8 +29,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.example.storeroom.util.StoreroomColor
 
 const val ADD_CATEGORY = "Add Category"
@@ -44,6 +49,7 @@ fun DropdownTextField(
     var expanded by remember { mutableStateOf(false) }
     var selectedIndex by remember { mutableIntStateOf(-1) }
     val updatedItems = listOf(ADD_CATEGORY) + items.filterNotNull()
+    var rowSize by remember { mutableStateOf(Size.Zero) }
 
     Column(
         modifier = Modifier.padding(horizontal = 32.dp)
@@ -56,9 +62,11 @@ fun DropdownTextField(
             border = BorderStroke(0.5.dp, Color(0xFFCCC9C9)),
             shape = RoundedCornerShape(16.dp),
         ) {
-            var offset = Offset.Zero
             Row(
                 modifier = Modifier
+                    .onGloballyPositioned { layoutCoordinates ->
+                        rowSize = layoutCoordinates.size.toSize()
+                    }
                     .fillMaxSize()
                     .clickable(onClick = { expanded = true })
                     .background(StoreroomColor.storeRoomDarkWhite),
@@ -79,7 +87,8 @@ fun DropdownTextField(
                 expanded = expanded,
                 onDismissRequest = { expanded = false },
                 modifier = Modifier
-                    .fillMaxWidth(fraction = 0.98f)
+                    .width(with(LocalDensity.current) { rowSize.width.toDp() })
+                    .heightIn(max = 300.dp)
                     .background(StoreroomColor.storeRoomDarkWhite)
             ) {
                 updatedItems.forEachIndexed { index, string ->
